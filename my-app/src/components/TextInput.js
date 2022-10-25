e6fb4cd650ca2752fc562df96ba92b5734651c45
Eliska Ryklova <ryklovae@coventry.ac.uk>
2022-10-25 17:07:07 +0000

    Login and Register pages for mobile

--- my-app/src/components/TextInput.js
+++ my-app/src/components/TextInput.js
@@ -2,16 +2,17 @@
 // TextInput.js
 
 import React from 'react';
-import { withStyles, makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import colours from '../colours.js';
 
 
 const useStyles = makeStyles((theme) => ({
-
-    
   textField: {
         width: '20ch',
+        ['@media (max-width:380px)']: { 
+            width: '15ch'
+        },
         "& .MuiFilledInput-input": {
             borderBottom: `3px solid ${colours.pink}`, 
             fontFamily: "Montserrat",
